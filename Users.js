92941bc9e4cf5010f847b6273ebbdae0a4277dc5
StefanieBelfast <49611105+StefanieBelfast@users.noreply.github.com>
2019-05-24 16:48:16 +0100

    update, still working on it

--- Users.js
+++ Users.js
@@ -8,12 +8,14 @@ describe('Users', () => {
     it('check items', () => {
         cy.get('.pageTitle').contains('UserTable')
         cy.get('.instructions').contains('List of accessible users')
+        cy.get('.userSearch')//.should('placeholder')
         cy.get('.checkmark').should('have.length', 3)
         cy.get('.shadedRow').should('have.length', 3)
         cy.get('.rightSeparator').should('have.length', 3)
           .contains('Dat Aowner')
-        cy.get('.rightSeparator').contains('seeder')
+        cy.get('.rightSeparator').contains('seeder').click()
         cy.get('.rightSeparator').contains('Ad Min')
+        cy.get('#resourcesButton').contains('Add new Resource')
         
         
     })
--- ValidationReports.js
+++ ValidationReports.js
@@ -1,11 +1,33 @@
 describe('Data Profiles', () => {
-    describe('LINTOL login', () => {
-        it('login', () => {
-            cy.visit('http://localhost:8000/login')
-            cy.contains('Local Admin').click()
-            cy.contains('Local Admin').click()
-        cy.contains('Data Profiles')
+
+    it('login', () => {
+        cy.visit('http://localhost:8000/login')
+        cy.contains('Local Admin').click()
+        cy.contains('Local Admin').click()
+        cy.contains('Validation Reports').click()
         
         })
+    it('check items/dropdown', () => {
+        cy.get('.pageTitle').contains('Validation Reports')
+        cy.get('.instructions').contains('Here is a list of generated reports for this account')
+          .wait(500)
+        cy.get('#dateFilter')//.click()
+        cy.get('#userFilter')//.click()
+        cy.get('#profileFilter')//.click()
+        /// dropdown menu
+        cy.get('.viewButton')//.click('id=[viewReport + 'report.name']')
+        cy.get('id=[viewReport + "report.name"]').click()
+        /// get the View Report
+    })
+
+    it('count items', () => {
+        cy.get('.rightSeparator').should('have.length', 5)
+        cy.get('.reportColumn').should('have.length', 20)
+        cy.get('.col-sm-4').should('have.length', 15)
+        cy.get('.shadedRow').should('have.length', 5)
+        cy.get('.ragError').should('have.length',5)
+        cy.get('.ragWarning').should('have.length',5)
+        cy.get('.ragPass').should('have.length',5)
+        cy.get('.ragNumber').should('have.length',15)
     })
 })
