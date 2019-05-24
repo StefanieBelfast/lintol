636515161bf193fce6dd4160d480f45d39dec968
StefanieBelfast <49611105+StefanieBelfast@users.noreply.github.com>
2019-05-24 16:47:44 +0100

    update, still working on it

--- DataProcessors.js
+++ DataProcessors.js
@@ -1,11 +1,25 @@
-describe('Data Profiles', () => {
-    describe('LINTOL login', () => {
+describe('DataProcessors', () => {
         it('login', () => {
             cy.visit('http://localhost:8000/login')
             cy.contains('Local Admin').click()
             cy.contains('Local Admin').click()
-        cy.contains('Data Profiles')
-        
+        cy.contains('Data Processors').click()
         })
+        it('check items', () => {
+            cy.get('.pageTitle').contains('Data Processors')
+            cy.get('.instructions').contains('These are the list of processors available to your profiles.')
+            cy.get('.processorSearch')//.should('placeholder')        
+    })
+    it('count items', () => {
+        cy.get('.rightSeparator').should('have.length', 5)
+          .contains('CSV Checking by GoodTables')
+        cy.get('.rightSeparator').contains('Boundary Checker (impr)')
+        cy.get('.description').should('have.length', 5)
+          .contains('Tool for searching for Personally-Identifiable Information within CSV data')
+        cy.get('.processorColumn').should('have.length', 15)
+        cy.get('.shadedRow').should('have.length', 5)
+        cy.get('.configurationsColumn').should('have.length',5)
+          .contains('Configurations')
+        
     })
 })
